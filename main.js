9f2a0a8a9ac6b2457ebc7fb145f7e603798b53c4
isaaccisaac2 <isaaccisaac2@gmail.com>
2025-04-01 10:47:56 +0000

    finalmente é possivel calcular tudo o que o projeto se propoem, contudo , ainda faltam coisas

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
  let tipPercentage = 0
  let numberOfPeople = 0
+ let buttonSelected = null
  
  let billInput = document.querySelector("#bill")
  billInput.addEventListener("input", receiveBillValue)
@@ -20,11 +21,40 @@ let bill = 0
  
  function receiveTipPercentage(value) {
      let buttonPercentage = document.querySelector()
-  }
+     if(buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
 
- function calculate(){
-     if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0 )
-         console.log("calcular")
-  } else{
-     console.log("ainda não é possivel calcular")
+    }
+
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+    tipPercentage = value / 100
+
+    let customTipInput = document.querySelector("#custom-tip")
+    customTipInput.value = ""
+
+    calculate()
+   }
+function receiveCustomTipPercentage(){
+    let customTipInput = document.querySelector("#custom-tip")
+    tipPercentage = customTipInput.valueAsNumber / 100
+
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null 
        }
+    calculate()
+      } 
+function calculate(){
+    if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0 ) {
+        let tipAmountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        tipAmountStrong.innerText = `$ ${tipAmountPerson.toFixed(2)}`
+        let totalStrong = document.querySelector(".total strong")
+        let total = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$ ${total.toFixed(2)}`
+       }
+          else{
+     console.log("ainda não é possivel calcular")
+        }
+      }
